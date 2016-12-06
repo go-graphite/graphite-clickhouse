b0f4b1be413afc1ed6d7f547cce415575f84259e
Roman Lomonosov <r.lomonosov@gmail.com>
2016-12-06 23:53:14 +0300

    checkpoint

--- backend/render.go
+++ backend/render.go
@@ -18,13 +18,6 @@ import (
 	"github.com/uber-go/zap"
 )
 
-type Point struct {
-	Metric    string
-	Time      int32
-	Value     float64
-	Timestamp int32 // keep max if metric and time equal on two points
-}
-
 type Points []Point
 
 func (s Points) Len() int      { return len(s) }
