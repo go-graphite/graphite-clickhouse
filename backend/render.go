5c4174590a48250ef7a47183c50300c9f21b5981
Vladimir Smirnov <vladimir.smirnov@booking.com>
2016-12-07 10:32:19 +0100

    Change PREWHERE back to WHERE
    PREWHERE in it's current state works worse for complex graphite queries,
    because it interfers with Clickhouse's own euristics.

--- backend/render.go
+++ backend/render.go
@@ -77,7 +77,7 @@ func (h *RenderHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		treeData, err := Query(
 			r.Context(),
 			h.config.ClickHouse.Url,
-			fmt.Sprintf("SELECT Path FROM %s PREWHERE %s GROUP BY Path", h.config.ClickHouse.TreeTable, treeWhere),
+			fmt.Sprintf("SELECT Path FROM %s WHERE %s GROUP BY Path", h.config.ClickHouse.TreeTable, treeWhere),
 			h.config.ClickHouse.TreeTimeout.Value(),
 		)
 
@@ -126,7 +126,7 @@ func (h *RenderHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		SELECT 
 			Path, Time, Value, Timestamp
 		FROM %s 
-		PREWHERE (%s) AND (%s)
+		WHERE (%s) AND (%s)
 		FORMAT RowBinary
 		`,
 		h.config.ClickHouse.DataTable,
