073b8f5ee5ff47725cf872ab740f6f4e5dab5810
Vladimir Smirnov <vladimir.smirnov@booking.com>
2016-12-08 13:14:53 +0100

    WIP: Implement Step function for rollup

--- helper/rollup/rollup_test.go
+++ helper/rollup/rollup_test.go
@@ -1,7 +1,9 @@
 package rollup
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/lomik/graphite-clickhouse/helper/point"
 )
@@ -71,5 +73,84 @@ func TestMetricPrecision(t *testing.T) {
 	for _, test := range tests {
 		result := doMetricPrecision(test[0], 60, AggrSum)
 		point.AssertListEq(t, test[1], result)
+	}
+}
+
+func TestMetricStep(t *testing.T) {
+	config := `
+<graphite_rollup>
+ 	<pattern>
+ 		<regexp>^metric\.</regexp>
+ 		<function>any</function>
+ 		<retention>
+ 			<age>0</age>
+ 			<precision>1</precision>
+ 		</retention>
+ 		<retention>
+ 			<age>3600</age>
+ 			<precision>10</precision>
+ 		</retention>
+ 	</pattern>
+ 	<default>
+ 		<function>max</function>
+ 		<retention>
+ 			<age>0</age>
+ 			<precision>60</precision>
+ 		</retention>
+ 		<retention>
+ 			<age>3600</age>
+ 			<precision>300</precision>
+ 		</retention>
+ 		<retention>
+ 			<age>86400</age>
+ 			<precision>3600</precision>
+ 		</retention>
+ 	</default>
+</graphite_rollup>
+`
+	r, err := ParseXML([]byte(config))
+	if err != nil {
+		t.Fatal(err)
+	}
+	now := int32(time.Now().Unix())
+
+	tests := []struct {
+		name         string
+		from         int32
+		expectedStep int32
+	}{
+		{
+			name:         "metric.foo1",
+			from:         now - 500,
+			expectedStep: 1,
+		},
+		{
+			name:         "metric.foo2",
+			from:         now - 3600,
+			expectedStep: 10,
+		},
+		{
+			name:         "foo.bar1",
+			from:         now - 500,
+			expectedStep: 60,
+		},
+		{
+			name:         "foo.bar2",
+			from:         now - 3700,
+			expectedStep: 300,
+		},
+		{
+			name:         "foo.bar3",
+			from:         now - 87000,
+			expectedStep: 3600,
+		},
+	}
+
+	for _, test := range tests {
+		fmt.Printf("Performing test for metric=%v (from=now-%v)\n", test.name, now-test.from)
+		step := r.Step(test.name, test.from)
+		if step != test.expectedStep {
+			t.Errorf("metric=%v (from=now-%v), expected step=%v, actual step=%v", test.name, now-test.from, test.expectedStep, step)
+		}
 	}
 }
