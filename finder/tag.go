e049897c81d8bf60fea4cc50e4a95c260fde2391
Roman Lomonosov <r.lomonosov@gmail.com>
2016-12-23 20:38:47 +0300

    checkpoint

--- finder/tag.go
+++ finder/tag.go
@@ -2,49 +2,208 @@ package finder
 
 import (
 	"context"
+	"fmt"
 	"strings"
 	"time"
+
+	"github.com/lomik/graphite-clickhouse/helper/clickhouse"
 )
 
-type TagFinder struct {
-	wrapped Finder
-	ctx     context.Context // for clickhouse.Query
-	url     string          // clickhouse dsn
-	table   string          // graphite_tag table
-	timeout time.Duration   // clickhouse query timeout
+type TagState int
+
+const (
+	TagRoot     TagState = iota // query = "*"
+	TagSkip                     // not _tag prefix
+	TagInfoRoot                 // query = "_tag"
+)
+
+type TagQ struct {
+	Param *string
+	Value *string
 }
 
-func WrapTag(f Finder, ctx context.Context, url string, table string, timeout time.Duration) Finder {
-	return &TagFinder{
-		wrapped: f,
-		ctx:     ctx,
-		url:     url,
-		table:   table,
-		timeout: timeout,
+func (q TagQ) String() string {
+	if q.Param != nil && q.Value != nil {
+		return fmt.Sprintf("{\"param\"=%#v, \"value\"=%#v}", *q.Param, *q.Value)
 	}
+	if q.Param != nil {
+		return fmt.Sprintf("{\"param\"=%#v}", *q.Param)
+	}
+	if q.Value != nil {
+		return fmt.Sprintf("{\"value\"=%#v}", *q.Value)
+	}
+	return "{}"
+}
+
+func (q *TagQ) Where(field string) string {
+	if q.Param != nil && q.Value != nil && *q.Value != "*" {
+		return fmt.Sprintf("%s='%s%s'", field, clickhouse.Escape(*q.Param), clickhouse.Escape(*q.Value))
+	}
+	if q.Param != nil {
+		return fmt.Sprintf("%s LIKE '%s%%'", field, clickhouse.Escape(*q.Param))
+	}
+	if q.Value != nil && *q.Value != "*" {
+		return fmt.Sprintf("%s='%s'", field, clickhouse.Escape(*q.Value))
+	}
+
+	return ""
+}
+
+type TagFinder struct {
+	wrapped     Finder
+	ctx         context.Context // for clickhouse.Query
+	url         string          // clickhouse dsn
+	table       string          // graphite_tag table
+	timeout     time.Duration   // clickhouse query timeout
+	state       TagState
+	tagQuery    []TagQ
+	seriesQuery string
+}
+
+var EmptyList [][]byte = [][]byte{}
+
+func WrapTag(f Finder, ctx context.Context, url string, table string, timeout time.Duration) *TagFinder {
+	return &TagFinder{
+		wrapped:  f,
+		ctx:      ctx,
+		url:      url,
+		table:    table,
+		timeout:  timeout,
+		tagQuery: make([]TagQ, 0),
+	}
+}
+
+func (t *TagFinder) tagListSQL() (string, error) {
+	if len(t.tagQuery) == 0 {
+		return "", nil
+	}
+
+	where := ""
+	and := func(condition string) {
+		if condition == "" {
+			return
+		}
+		if where != "" {
+			where = fmt.Sprintf("%s AND (%s)", where, condition)
+		} else {
+			where = fmt.Sprintf("(%s)", condition)
+		}
+	}
+
+	and("Level=1")
+
+	and(t.tagQuery[0].Where("Tag1"))
+
+	if len(t.tagQuery) == 1 {
+		return fmt.Sprintf("SELECT Tag1 FROM %s WHERE %s GROUP BY Tag1", t.table, where), nil
+	}
+
+	return where, nil
+}
+
+func (t *TagFinder) MakeSQL(query string) (string, error) {
+	if query == "_tag" {
+		return "", nil
+	}
+
+	qs := strings.Split(query, ".")
+
+	t.tagQuery = make([]TagQ, 0)
+
+	for {
+		if len(qs) == 0 {
+			break
+		}
+		if qs[0] == "_tag" {
+			if len(qs) >= 2 {
+				v := qs[1]
+				if len(v) > 0 && v[len(v)-1] == '=' {
+					if len(qs) >= 3 {
+						t.tagQuery = append(t.tagQuery, TagQ{Param: &v, Value: &qs[2]})
+						qs = qs[3:]
+					} else {
+						t.tagQuery = append(t.tagQuery, TagQ{Param: &v})
+						qs = qs[2:]
+					}
+				} else {
+					t.tagQuery = append(t.tagQuery, TagQ{Value: &v})
+					qs = qs[2:]
+				}
+			} else {
+				t.tagQuery = append(t.tagQuery, TagQ{})
+				qs = qs[1:]
+			}
+		} else {
+			t.seriesQuery = strings.Join(qs, ".")
+			break
+		}
+	}
+
+	if t.seriesQuery == "" {
+		return t.tagListSQL()
+	}
+
+	// where := ""
+
+	// AND := func(where string) string {
+	// 	if q
+	// }
+
+	return "", nil
 }
 
 func (t *TagFinder) Execute(query string) error {
-	qs := strings.Split(query, ".")
-	if len(qs) == 0 {
-		return nil
-	}
+	t.state = TagSkip
 
-	if qs[0] != "_tag" {
+	if query == "" {
 		return t.wrapped.Execute(query)
 	}
+
+	if query == "*" {
+		t.state = TagRoot
+		return t.wrapped.Execute(query)
+	}
+
+	if !strings.HasPrefix(query, "_tag.") {
+		return t.wrapped.Execute(query)
+	}
+
+	t.MakeSQL(query)
 
 	return nil
 }
 
 func (t *TagFinder) List() [][]byte {
-	return nil
+	switch t.state {
+	case TagSkip:
+		return t.wrapped.List()
+	case TagInfoRoot:
+		return [][]byte{[]byte("_tag.")}
+	case TagRoot:
+		// pass
+		return append([][]byte{[]byte("_tag.")}, t.wrapped.List()...)
+	default:
+		return EmptyList
+	}
+
+	return EmptyList
 }
 
 func (t *TagFinder) Series() [][]byte {
-	return nil
+	switch t.state {
+	case TagSkip:
+		return t.wrapped.List()
+	case TagInfoRoot:
+		return EmptyList
+	case TagRoot:
+		return t.wrapped.Series()
+	default:
+		return EmptyList
+	}
+
+	return EmptyList
 }
 
-func (t *TagFinder) Abs([]byte) []byte {
-	return nil
+func (t *TagFinder) Abs(v []byte) []byte {
+	return v
 }
--- finder/tag_test.go
+++ finder/tag_test.go
@@ -11,16 +11,77 @@ import (
 	"github.com/lomik/graphite-clickhouse/helper/clickhouse"
 )
 
-func TestTagFinderExecute(t *testing.T) {
+func TestTagsMakeSQL(t *testing.T) {
 	assert := assert.New(t)
 
 	table := []struct {
-		query         string
-		expectedQuery string
-		expectedError bool
+		query string
+		sql   string
+		error bool
 	}{
-		{"*", "", false},
-		{"_tag.*", "", false},
+		{"_tag", "", false},
+		{"_tag.*", "SELECT Tag1 FROM table WHERE (Level=1) GROUP BY Tag1", false},
+		{"_tag.t1", "SELECT Tag1 FROM table WHERE (Level=1) AND (Tag1='t1') GROUP BY Tag1", false},
+		{"_tag.p1=", "SELECT Tag1 FROM table WHERE (Level=1) AND (Tag1 LIKE 'p1=%') GROUP BY Tag1", false},
+		{"_tag.p1=.*", "SELECT Tag1 FROM table WHERE (Level=1) AND (Tag1 LIKE 'p1=%') GROUP BY Tag1", false},
+		{"_tag.p1=.v1", "SELECT Tag1 FROM table WHERE (Level=1) AND (Tag1='p1=v1') GROUP BY Tag1", false},
+	}
+
+	for _, test := range table {
+		testName := fmt.Sprintf("query: %#v", test.query)
+
+		m := NewMockFinder([][]byte{[]byte("mock")})
+		f := WrapTag(m, context.Background(), "http://localhost:8123/", "table", time.Second)
+
+		sql, err := f.MakeSQL(test.query)
+
+		if test.error {
+			assert.Error(err)
+		} else {
+			assert.NoError(err)
+		}
+		assert.Equal(test.sql, sql, testName)
+	}
+}
+
+func _TestTags(t *testing.T) {
+	assert := assert.New(t)
+
+	mockData := [][]byte{[]byte("mock")}
+
+	type w []string
+	mock := w{"mock"}
+	empty := w{}
+
+	table := []struct {
+		query          string
+		expectedList   []string
+		expectedSeries []string
+	}{
+		// not tagged query
+		{"", mock, mock},
+		{"t*", mock, mock},
+		{"hello.*", mock, mock},
+
+		// list root
+		{"*", w{"_tag.", "mock"}, mock},
+
+		// info about _tag "directory"
+		{"_tag", w{"_tag."}, empty},
+		{"_tag.*", w{"_tag.t1.", "_tag.t2."}, empty},
+		{"_tag.t1", w{"_tag.t1.", "_tag.t2."}, empty},
+		{"_tag.t1.*", w{"_tag.t1.", "_tag.t2."}, empty},
+		{"_tag.t1._tag.*", w{"_tag.t1.", "_tag.t2."}, empty},
+		{"_tag.t1._tag.param=", w{"_tag.t1.", "_tag.t2."}, empty},
+		{"_tag.t1._tag.param=.value", w{"_tag.t1.", "_tag.t2."}, empty},
+		{"_tag.t1._tag.param=.value.*", w{"_tag.t1.", "_tag.t2."}, empty},
+
+		// {"hello", []string{"hello."}, []string{}},
+		// {"hello.*", []string{"hello.world"}, []string{"world"}},
+		// {"*.*", []string{"hello.world"}, []string{"world"}},
+		// {"*404*", []string{}, []string{}},
+		// {"*404*.*", []string{}, []string{}},
+		// {"hello.[bad regexp", []string{}, []string{}},
 	}
 
 	for _, test := range table {
@@ -28,25 +89,24 @@ func TestTagFinderExecute(t *testing.T) {
 
 		srv := clickhouse.NewTestServer()
 
-		m := NewMockFinder([][]byte{})
+		m := NewMockFinder(mockData)
 		f := WrapTag(m, context.Background(), srv.URL, "graphite_tag", time.Second)
 
-		srv.Close()
+		f.Execute(test.query)
 
-		err := f.Execute(test.query)
-
-		if test.expectedError {
-			assert.Error(err, testName)
-		} else {
-			assert.NoError(err, testName)
+		list := make([]string, 0)
+		for _, r := range f.List() {
+			list = append(list, string(r))
 		}
 
-		requests := srv.Requests()
+		series := make([]string, 0)
+		for _, r := range f.Series() {
+			series = append(series, string(r))
+		}
 
-		fmt.Println(requests)
+		assert.Equal(test.expectedList, list, testName+", list")
+		assert.Equal(test.expectedSeries, series, testName+", series")
 
-		// assert.Equal(test.expectedQ, m.query, testName)
-		// assert.Equal(test.expectedMatched, f.matched, testName)
-		// assert.Equal(test.expectedPart, f.part, testName)
+		srv.Close()
 	}
 }
