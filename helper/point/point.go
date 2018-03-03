58f42149506b393deb2bca0e9237a67b235e2c4b
Roman Lomonosov <r.lomonosov@gmail.com>
2018-03-03 14:18:52 +0300

    point struct size

--- helper/point/point.go
+++ helper/point/point.go
@@ -2,7 +2,7 @@ package point
 
 type Point struct {
 	Metric    string
-	Time      int32
 	Value     float64
+	Time      int32
 	Timestamp int32 // keep max if metric and time equal on two points
 }
