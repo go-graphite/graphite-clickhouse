01cd45ba99c41337c1d5b1f0142bd16b5348f612
Safronov Michail <msaf1980@gmail.com>
2019-07-02 10:09:02 +0500

    check find query param

--- find/handler.go
+++ find/handler.go
@@ -20,7 +20,12 @@ func NewHandler(config *config.Config) *Handler {
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	r.ParseMultipartForm(1024 * 1024)
 
-	f, err := New(h.config, r.Context(), r.FormValue("query"))
+	query := r.FormValue("query")
+	if len(query) == 0 {
+		http.Error(w, "Query not set", http.StatusBadRequest)
+		return
+	}
+	f, err := New(h.config, r.Context(), query)
 	if err != nil {
 		clickhouse.HandleError(w, err)
 		return
