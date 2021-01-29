a929e137f335ee266f03cd153db577961ac2bf4a
Thomas Casteleyn <thomas.casteleyn@super-visions.com>
2021-01-29 09:23:43 +0100

    Change to Sprintf usage

--- autocomplete/autocomplete.go
+++ autocomplete/autocomplete.go
@@ -225,10 +225,10 @@ func (h *Handler) ServeValues(w http.ResponseWriter, r *http.Request) {
 
 	var valueSQL string
 	if len(usedTags) == 0 {
-		valueSQL = "substr(Tag1, " + (len(tag)+2) + ") AS value"
+		valueSQL = fmt.Sprintf("substr(Tag1, %d) AS value", len(tag)+2)
 		wr.And(where.HasPrefix("Tag1", tag+"="+valuePrefix))
 	} else {
-		valueSQL = "substr(arrayJoin(Tags), " + (len(tag)+2) + ") AS value"
+		valueSQL = fmt.Sprintf("substr(arrayJoin(Tags), %d) AS value", len(tag)+2)
 		wr.And(where.HasPrefix("arrayJoin(Tags)", tag+"="+valuePrefix))
 	}
 
