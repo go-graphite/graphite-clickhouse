4724dd370ac26463a854b539ac36c8ebb42db3d5
msaf1980 <msaf1980@gmail.com>
2021-04-21 06:41:26 +0500

    tests for clear glob

--- pkg/where/match_test.go
+++ pkg/where/match_test.go
@@ -11,7 +11,7 @@ func TestGlob(t *testing.T) {
 		{"a.{a,b}.te{s}t.b", "test LIKE 'a.%' AND match(test, '^a[.](a|b)[.]test[.]b$')"},
 		{"a.{a,b}.te{s,t}*.b", "test LIKE 'a.%' AND match(test, '^a[.](a|b)[.]te(s|t)([^.]*?)[.]b$')"},
 		{"a.{a,b}.test*.b", "test LIKE 'a.%' AND match(test, '^a[.](a|b)[.]test([^.]*?)[.]b$')"},
-		{"a.[b].te{s}t.b", "test LIKE 'a.%' AND match(test, '^a[.][b][.]test[.]b$')"},
+		{"a.[b].te{s}t.b", "test='a.b.test.b'"},
 		{"a.[ab].te{s,t}*.b", "test LIKE 'a.%' AND match(test, '^a[.][ab][.]te(s|t)([^.]*?)[.]b$')"},
 	}
 	for _, tt := range tests {
