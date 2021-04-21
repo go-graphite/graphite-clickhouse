ca4df84f747e64bcd33d7d60816abbd19c38d063
msaf1980 <msaf1980@gmail.com>
2021-04-21 09:52:12 +0500

    test for reverse index usage tune with regex

--- finder/index_test.go
+++ finder/index_test.go
@@ -24,7 +24,7 @@ func Test_useReverse(t *testing.T) {
 	}
 
 	for _, tt := range table {
-		assert.Equal(tt.result, useReverseDepth(tt.query, 1, []config.NValue{}), tt.query)
+		assert.Equal(tt.result, useReverseDepth(tt.query, 1, []*config.NValue{}), tt.query)
 	}
 }
 
@@ -49,17 +49,18 @@ func Test_useReverseWithFixedDepth(t *testing.T) {
 	}
 
 	for _, tt := range table {
-		assert.Equal(tt.result, useReverseDepth(tt.query, tt.depth, []config.NValue{}), fmt.Sprintf("%s with depth %d", tt.query, tt.depth))
+		assert.Equal(tt.result, useReverseDepth(tt.query, tt.depth, []*config.NValue{}), fmt.Sprintf("%s with depth %d", tt.query, tt.depth))
 	}
 }
 
 func Test_useReverseDepth(t *testing.T) {
 	assert := assert.New(t)
 
-	usesDepth := []config.NValue{
-		{Suffix: ".sum", Value: 2},
-		{Prefix: "test.", Suffix: ".alloc", Value: 2},
-		{Prefix: "test2.", Value: 2},
+	usesDepth := []*config.NValue{
+		&config.NValue{Suffix: ".sum", Value: 2},
+		&config.NValue{Prefix: "test.", Suffix: ".alloc", Value: 2},
+		&config.NValue{Prefix: "test2.", Value: 2},
+		&config.NValue{RegexStr: `^a\..*\.max$`, Value: 2},
 	}
 
 	table := []struct {
@@ -73,9 +74,43 @@ func Test_useReverseDepth(t *testing.T) {
 		{"test.b.c*.d.alloc", 1, true},
 		{"test2.b.c*.d*.e", 1, false},
 		{"test2.b.c*.d.e", 1, true},
+		{"a.b.c.d*.max", 1, false}, // regex test
+		{"a.b.c*.d.max", 1, true},  // regex test
 	}
+
+	assert.NoError(config.IndexUseReversesValidate(usesDepth))
 
 	for _, tt := range table {
 		assert.Equal(tt.result, useReverseDepth(tt.query, tt.depth, usesDepth), fmt.Sprintf("%s with depth %d", tt.query, tt.depth))
+	}
+}
+
+func Benchmark_useReverseDepth(b *testing.B) {
+	usesDepth := []*config.NValue{
+		&config.NValue{Prefix: "test2.", Value: 2},
+	}
+
+	for i := 0; i < b.N; i++ {
+		_ = useReverseDepth("test2.b.c*.d.e", 1, usesDepth)
+	}
+}
+
+func Benchmark_useReverseDepthPrefixSuffix(b *testing.B) {
+	usesDepth := []*config.NValue{
+		&config.NValue{Prefix: "test2.", Suffix: ".e", Value: 2},
+	}
+
+	for i := 0; i < b.N; i++ {
+		_ = useReverseDepth("test2.b.c*.d.e", 1, usesDepth)
+	}
+}
+
+func Benchmark_useReverseDepthRegex(b *testing.B) {
+	usesDepth := []*config.NValue{
+		&config.NValue{RegexStr: `^a\..*\.max$`, Value: 2},
+	}
+
+	for i := 0; i < b.N; i++ {
+		_ = useReverseDepth("a.b.c*.d.max", 1, usesDepth)
 	}
 }
