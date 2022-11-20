c7034661fdcc65669ff990858d9fc239a4c4dab9
lomik <r.lomonosov@gmail.com>
2022-11-20 10:22:00 +0300

    Revert "remove unused page-title, add listen addr"
    This reverts commit e9dc3442afabb11470034c3d92800a41706d7d6c.

--- config/config.go
+++ config/config.go
@@ -181,6 +181,7 @@ type Prometheus struct {
 	Listen         string   `toml:"listen" json:"listen" comment:"listen addr for prometheus ui and api"`
 	ExternalURLRaw string   `toml:"external-url" json:"external-url" comment:"allows to set URL for redirect manually"`
 	ExternalURL    *url.URL `toml:"-" json:"-"`
+	PageTitle      string   `toml:"page-title" json:"page-title"`
 }
 
 const (
@@ -289,7 +290,7 @@ func New() *Config {
 		},
 		Prometheus: Prometheus{
 			ExternalURLRaw: "",
-			Listen:         ":9090",
+			PageTitle:      "Prometheus Time Series Collection and Processing Server",
 		},
 		Debug: Debug{
 			Directory:        "",
