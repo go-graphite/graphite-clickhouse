ada271d2d6e57233eadd2844c58b99f0e9cd42b9
Mikhail f. Shiryaev <mr.felixoid@gmail.com>
2023-12-20 13:43:19 +0100

    Clean up vendor_prometheus_ui.sh a little bit

--- vendor_prometheus_ui.sh
+++ vendor_prometheus_ui.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
+set -e
 
-cd $(dirname $0)
+cd "$(dirname "$0")"
 
-BRANCH=$(cat go.mod  | grep 'github.com/prometheus/prometheus' | awk '{print $2}')
+BRANCH=$(grep 'github.com/prometheus/prometheus' go.mod | awk '{print $2}')
 
-rm -rf tmp || exit 1
-mkdir tmp || exit 1
-git clone --depth 1 --branch $BRANCH https://github.com/prometheus/prometheus.git tmp || exit 1
+rm -rf tmp
+mkdir tmp
+git clone --depth 1 --branch "$BRANCH" https://github.com/prometheus/prometheus.git tmp
 
-cd tmp || exit 1
-make assets-compress || exit 1
+cd tmp
+make assets-compress
 
-cd .. || exit 1
+cd ..
 
-cp tmp/web/ui/embed.go vendor/github.com/prometheus/prometheus/web/ui/embed.go || exit 1
-rm -rf vendor/github.com/prometheus/prometheus/web/ui/static || exit 1
-cp -a tmp/web/ui/static vendor/github.com/prometheus/prometheus/web/ui/static || exit 1
-rm -rf vendor/github.com/prometheus/prometheus/web/ui/.gitignore tmp || exit 1
+cp tmp/web/ui/embed.go vendor/github.com/prometheus/prometheus/web/ui/embed.go
+rm -rf vendor/github.com/prometheus/prometheus/web/ui/static
+cp -a tmp/web/ui/static vendor/github.com/prometheus/prometheus/web/ui/static
+rm -rf vendor/github.com/prometheus/prometheus/web/ui/.gitignore tmp
