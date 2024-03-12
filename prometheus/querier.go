16307d1d51cf700f0a261c2acff1eabe61407502
Florian Derler <f.derler@conova.com>
2024-03-12 17:00:15 +0100

    add label filtering support to /api/v1/label*

--- prometheus/querier.go
+++ prometheus/querier.go
@@ -13,9 +13,11 @@ import (
 	"github.com/prometheus/prometheus/storage"
 
 	"github.com/lomik/graphite-clickhouse/config"
+	"github.com/lomik/graphite-clickhouse/finder"
 	"github.com/lomik/graphite-clickhouse/helper/clickhouse"
 	"github.com/lomik/graphite-clickhouse/pkg/scope"
 	"github.com/lomik/graphite-clickhouse/pkg/where"
+	// "github.com/lomik/graphite-clickhouse/pkg/where"
 )
 
 // Querier provides reading access to time series data.
@@ -34,15 +36,33 @@ func (q *Querier) Close() error {
 // LabelValues returns all potential values for a label name.
 func (q *Querier) LabelValues(label string, matchers ...*labels.Matcher) ([]string, storage.Warnings, error) {
 	// @TODO: support matchers
-	w := where.New()
-	w.And(where.HasPrefix("Tag1", label+"="))
+	terms := []finder.TaggedTerm{
+		{
+			Key:         label,
+			Op:          finder.TaggedTermEq,
+			Value:       "*",
+			HasWildcard: true,
+		},
+	}
+
+	matcherTerms, err := makeTaggedFromPromQL(matchers)
+	if err != nil {
+		return nil, nil, err
+	}
+	terms = append(terms, matcherTerms...)
+	
+	wr, _, err := finder.TaggedWhere(terms)
+	if err != nil {
+		return nil, nil, err
+	}
+	//wr.And(where.HasPrefix("Tag1", label+"="))
 
 	fromDate := timeNow().AddDate(0, 0, -q.config.ClickHouse.TaggedAutocompleDays)
-	w.Andf("Date >= '%s'", fromDate.Format("2006-01-02"))
+	wr.Andf("Date >= '%s'", fromDate.Format("2006-01-02"))
 
 	sql := fmt.Sprintf("SELECT splitByChar('=', Tag1)[2] as value FROM %s %s GROUP BY value ORDER BY value",
 		q.config.ClickHouse.TaggedTable,
-		w.SQL(),
+		wr.SQL(),
 	)
 
 	body, _, _, err := clickhouse.Query(
@@ -71,7 +91,18 @@ func (q *Querier) LabelValues(label string, matchers ...*labels.Matcher) ([]stri
 // LabelNames returns all the unique label names present in the block in sorted order.
 func (q *Querier) LabelNames(matchers ...*labels.Matcher) ([]string, storage.Warnings, error) {
 	// @TODO support matchers
+	terms, err := makeTaggedFromPromQL(matchers)
+	if err != nil {
+		return nil, nil, err
+	}
 	w := where.New()
+	for i := 0; i < len(terms); i++ {
+		and, err := finder.TaggedTermWhereN(&terms[i])
+		if err != nil {
+			return nil, nil, err
+		}
+		w.And(and)
+	}
 	fromDate := time.Now().AddDate(0, 0, -q.config.ClickHouse.TaggedAutocompleDays).UTC()
 	w.Andf("Date >= '%s'", fromDate.Format("2006-01-02"))
 
